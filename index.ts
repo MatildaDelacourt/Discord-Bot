42ac15ffbc5b61365d93d767a8822d6f99acac3c
MatildaDelacourt <matilda.delacourt@gmail.com>
2021-11-07 21:00:59 -0500

    add prefix and arguments

--- index.ts
+++ index.ts
@@ -4,6 +4,9 @@ const { Intents } = require('discord.js');
 const DiscordJS = require('discord.js');
 const dotenv = require('dotenv');
 dotenv.config();
+// Getting the prefix
+const PREFIX = String(process.env.PREFIX);
+console.log('prefix: ', PREFIX.length);
 
 // creates client
 const client = new DiscordJS.Client({
@@ -18,20 +21,43 @@ client.on('ready', () => {
 //Create message, if message is cowsay give a reaction or a reply
 //and catch error if there is any.
 client.on('messageCreate', (message: any) => {
-  if (message.content === 'cowsay') {
-    message.react('🚔').then(console.log).catch(console.error);
-    const output = cowsay();
-    message
-      .reply(output)
-      .then(function (response: any) {
-        console.log(`Replied to message "${message.content}"`);
-      })
-      .catch(function (response: any) {
-        if (response['httpStatus'] == 400) {
-          message.reply('ERROR: image is too big');
-        }
-      });
+  console.log('testing: ', message.content);
+
+  if (!message.content.startsWith(PREFIX)) return;
+  // Checks the first 3 characters of the message.content
+  console.log('no return: ', message.content);
+  const args = message.content
+    .toLowerCase()
+    .substring(PREFIX.length)
+    .slice()
+    .split(/ /)[2];
+  // Valid commands
+  let validCommands = ['cat', 'atom'];
+  // Checking to see that the argument after md# cowsay matches the accepted faces and is valid.
+  const isValid = validCommands.includes(args);
+
+  console.log('isValid: ', isValid);
+
+  if (!isValid) {
+    message.reply('ERROR: valid commands are cat or atom ONLY');
+    console.log(`You entered an unknown command.`);
+    return;
   }
+  // reaction to a message
+  message.react('🚔').then(console.log).catch(console.error);
+  // taking a variable for cowsay function
+  const output = cowsay(String(args));
+  // reply to message and catching error if there is any
+  message
+    .reply(output)
+    .then(function (response: any) {
+      console.log(`Replied to message "${message.content}"`);
+    })
+    .catch(function (response: any) {
+      if (response['httpStatus'] == 400) {
+        message.reply('ERROR: image is too big');
+      }
+    });
 });
 // login bot
 client.login(process.env.TOKEN);
--- utils/cowsay.ts
+++ utils/cowsay.ts
@@ -5,21 +5,20 @@ import getRandomInt from './random';
 import quotes from './quotes.json';
 
 //export cowsay
-export default function () {
+export default function (face: String) {
   //Getting a random value from 0 to 25. The length of quotes array.
   let rando = getRandomInt(0, 25);
   //import { iOptions } for cowsay
   let opts: typeof IOptions = {
-    //Getting the random quote, author and image.
+    //Getting the random quote and author. Changes cow face.
     text: quotes[rando].quote + ' - ' + quotes[rando].author,
     e: '^^',
-    r: true,
-    //f: '',
+    r: false,
+    f: face,
   };
 
   //new cow
   let output: string = cowsay.say(opts);
-  console.log(output);
 
   //returns the cow
   return output;
